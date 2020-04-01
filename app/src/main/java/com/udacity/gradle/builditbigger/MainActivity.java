662ff2591e261d845d6549d2eefa50b0caa710a5
Hangbo <zhzhanghangbo@hotmail.com>
2020-03-31 22:15:25 -0400

    Creat a java library used for providing jokes

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -6,9 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import com.prisonerprice.jokelib.Joker;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static Joker joker = Joker.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, joker.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ jokelib/src/main/java/com/prisonerprice/jokelib/Joker.java
@@ -0,0 +1,40 @@
+package com.prisonerprice.jokelib;
+
+/*
+Joke Reference: https://www.rd.com/funny-stuff/short-jokes/
+ */
+
+import java.util.Random;
+
+public class Joker {
+
+    private static Joker joker;
+    private static String[] jokes;
+
+    private Joker() {
+        jokes = new String[]{
+                "What's the best thing about Switzerland?\nI don't know, but the flag is a big plus.",
+                "I invented a new word!\nPlagiarism!",
+                "Did you hear about the mathematician who's afraid of negative numbers?\nHe will stop at nothing to avoid them.",
+                "Why do we tell actors to break a leg?\nBecause every play has a cast.",
+                "Helvetica and Times New Roman walk into a bar.\n\"Get out of here!\"shouts the bartender.\"We don't serve your type!\"",
+                "Yesterday I saw a guy spill all his Scrabble letters on the road.\nI asked him, \"What's the word on the street?\"",
+                "Here about the new restaurant called Karma?\nThere's no menu: You get what you deserve.",
+                "Why don't scientists trust atoms?\nBecause they make up everything."
+        };
+    }
+
+    public static Joker getInstance() {
+        if (joker == null) {
+            joker = new Joker();
+        }
+        return joker;
+    }
+
+    public static String getJoke(){
+        int length = jokes.length;
+        Random random = new Random();
+        int r = random.nextInt(length);
+        return jokes[r];
+    }
+}
